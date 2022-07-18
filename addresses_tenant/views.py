7522032349f090f1b83de62fbfcdfc0f75dcacd5
DorianMoreno <dorian.r.mv@gmail.com>
2022-07-18 04:10:36 -0500

    Update views.py

--- addresses_tenant/views.py
+++ addresses_tenant/views.py
@@ -1,14 +1,23 @@
 from django.shortcuts import render
 from django.conf import settings
+from ./models import Address
 
-'''
-Basic view for displaying a map 
-'''
+def address_detail():
+    address_objs = Address.objects.get()
+
+    context = {
+        "addresses": address_objs,
+    }
+
+    return render(request, 'addresses_tenant/map.html', context)
+
 def map(request):
 
 	name = request.GET.get("name")
 	lat = request.GET.get("lat")
 	lon = request.GET.get("lon")
+
+    print(name)
 
 	context = {
 	"google_api_key": settings.GOOGLE_API_KEY,
