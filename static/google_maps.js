9dab16510b1037f50660cd5186971570aef7660f
DorianMoreno <dorian.r.mv@gmail.com>
2022-07-18 05:05:29 -0500

    Modified parameter addresses

--- static/google_maps.js
+++ static/google_maps.js
@@ -9,7 +9,7 @@ $.getScript( "https://maps.googleapis.com/maps/api/js?key=" + google_api_key + "
 function initMap() {
     console.log("Initializing Map");
     var map = new google.maps.Map(document.getElementById('map-route'), {
-        center: { lat: add[0].latitude, lng: add[0].longitude },
+        center: { lat: addresses[0].latitude, lng: addresses[0].longitude },
         zoom: 8,
     });
     console.log("Map is ready");
