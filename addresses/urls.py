a8a23e7ebb0d3714fe137fb06c46a3445754236e
DorianMoreno <dorian.r.mv@gmail.com>
2022-07-18 01:17:18 -0500

    Applying Maps example

--- addresses/urls.py
+++ addresses/urls.py
@@ -16,9 +16,12 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from django.views.generic.base import TemplateView
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include("django.contrib.auth.urls")),
+    path('addresses', include('addresses_tenant.urls', namespace='atenant')),
     path('', TemplateView.as_view(template_name='home.html'), name='home'),
 ]
--- addresses_tenant/views.py
+++ addresses_tenant/views.py
@@ -1,3 +1,34 @@
 from django.shortcuts import render
+from django.conf import settings
 
-# Create your views here.
+from .mixins import Directions
+'''
+Basic view for routing 
+'''
+def route(request):
+
+	context = {"google_api_key": settings.GOOGLE_API_KEY}
+	return render(request, 'atenant/route.html', context)
+
+'''
+Basic view for displaying a map 
+'''
+def map(request):
+
+	lat_a = request.GET.get("lat_a")
+	long_a = request.GET.get("long_a")
+	lat_b = request.GET.get("lat_b")
+	long_b = request.GET.get("long_b")
+	directions = Directions(
+		lat_a= lat_a,
+		long_a=long_a,
+		lat_b = lat_b,
+		long_b=long_b
+		)
+
+	context = {
+	"google_api_key": settings.GOOGLE_API_KEY,
+	"lat": lat_a,
+	"long": long_a,
+	}
+	return render(request, 'atenant/map.html', context)
